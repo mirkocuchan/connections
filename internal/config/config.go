90df9bea7f74cdc7e2283ce0d4b0a965e14cf96c
mirkocuchan <mirko.cuchan01@gmail.com>
2026-07-27 02:09:49 +0000

    config file, state and starting with the idea of handlers

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,23 @@
 package config
 
 import (
-	"encoding/json"
 	"os"
-	"path/filepath"
+	"errors"
 )
 
 type Config struct {
 	DBURL string
+}
+
+func GetConfig() (Config, error){
+	//getting the dbURL, in this case from the .env to open the connection
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		return Config{}, errors.New("error reading .env")
+	}
+
+	newConfig := Config{
+		DBURL: dbURL,
+	}
+	return newConfig, nil
 }
--- main.go
+++ main.go
@@ -3,16 +3,25 @@ package main
 import _ "github.com/lib/pq"
 import(
 	"database/sql"
+	"github.com/mirkocuchan/connections/internal/config"
+	"github.com/mirkocuchan/connections/internal/database"
 	"log"
 )
 //la estructura va a guardar el tipo "*database.Queries" que es mi dbQueries
 type state struct {
     db  *database.Queries
+	cfg *config.Config
 }
 
 func main(){
+	//getting the dbURL from the config
+	cfg, err := config.GetConfig()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	
 	//db es del tipo *sql.DB, implementa a la interfaz DBTX. creamos la conexion y nos queda en db
-	db, err := sql.Open("postgres", dbURL)
+	db, err := sql.Open("postgres", cfg.DBURL)
 	if err != nil {
 		log.Fatalf("error connecting to db: %v", err)
 	}
@@ -20,6 +29,10 @@ func main(){
 
 	//luego, creamos un objeto queries que es una estructura que guarda la db connection
 	dbQueries := database.New(db)
-
+	//definimos el estado del programa con la config y la db
+	programState := &state{
+		db:  dbQueries,
+		cfg: &cfg,
+	}
 
 }
