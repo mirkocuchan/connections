cacef4cec5c3ea33cf93c0056e3ede251e434121
mirkocuchan <mirko.cuchan01@gmail.com>
2026-08-07 01:52:18 +0000

    loading everything into the config struct. todo: .env setting variables with this command (openssl rand -base64 32)

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,11 +5,15 @@ import (
 	"errors"
 	"github.com/joho/godotenv"
 	"log"
+	"time"
 )
 
 type Config struct {
 	DBURL string
 	PORT string
+	JWTSecret string
+	AccessTokenDuration time.Duration
+	RefreshTokenDuration time.Duration
 }
 
 func GetConfig() (Config, error){
@@ -30,9 +34,25 @@ func GetConfig() (Config, error){
 		return Config{}, errors.New("PORT environment variable is not set")
 	}
 
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		return Config{}, errors.New("JWT_SECRET environment variable is not set")
+	}
+	accessTokenDuration, accessErr := time.ParseDuration(os.Getenv("ACCESS_TOKEN_DURATION"))
+	refreshTokenDuration, refreshErr := time.ParseDuration(os.Getenv("REFRESH_TOKEN_DURATION"))
+	if accessErr != nil{
+		return Config{}, errors.New("ACCESS_TOKEN_DURATION environment variable is not set")
+	}
+	if refreshErr != nil{
+		return Config{}, errors.New("REFRESH_TOKEN_DURATION environment variable is not set")
+	}
+
 	config := Config{
 		DBURL: dbURL,
 		PORT: port,
+		JWTSecret: jwtSecret,
+		AccessTokenDuration: accessTokenDuration,
+		RefreshTokenDuration: refreshTokenDuration,
 	}
 	return config, nil
 }
