186040787b9a778bc0152488aeff9f2486c72319
mirkocuchan <mirko.cuchan01@gmail.com>
2026-08-12 18:01:34 +0000

    some minor errors fixed

--- middlewares.go
+++ middlewares.go
@@ -1,3 +1,17 @@
+package main
+
+import(
+	"net/http"
+	"github.com/mirkocuchan/connections/internal/auth"
+	"encoding/json"
+	"github.com/google/uuid"
+	"context"
+	"github.com/lib/pq"
+)
+type contextKey string
+
+const userIDKey contextKey = "user_id"
+
 //el middleware toma un handler, y lo devuelve, habiendo ejecutado la funcion interna del middleware antes de ejecutar el handler. 
 //el middleware es un wrapper que envuelve al handler, y le agrega funcionalidad antes o despues de ejecutar el handler.
 func (s *state) authMiddleware(next http.Handler) http.Handler {
@@ -16,8 +30,8 @@ func (s *state) authMiddleware(next http.Handler) http.Handler {
 		//middleware que solo valida el jwt. si es valido el jwt, llamo al proximo handler. 
 		
 		//hago que el userID esté disponible para el handler que se ejecuta después del middleware, agregándolo al contexto de la request.
-		//entonces uso el middleware para ahorrar el trabajo de parsear el jwt en cada handler que lo necesite. el middleware lo hace una sola vez y lo pone en el contexto de la request.
-		ctx := context.WithValue(r.Context(), "user_id", userID)
+		//entonces uso el middleware para ahorrar el trabajo de parsear el jwt en cada handler que lo necesite. el middleware lo hace una sola vez y lo pone en el contexto de la request para que los handlers posteriores puedan acceder a él. el middleware es un wrapper que envuelve al handler, y le agrega funcionalidad antes o despues de ejecutar el handler.
+		ctx := context.WithValue(r.Context(), userIDKey, userID)
 		r = r.WithContext(ctx)
 
 		next.ServeHTTP(w, r)
