4f7383ee8f3232d0d7b8c74759f229af1ed05a71
mirkocuchan <mirko.cuchan01@gmail.com>
2026-08-24 20:22:49 +0000

    migration redone, changes to tables

--- sql/schema/005_usercards.sql
+++ sql/schema/005_usercards.sql
@@ -20,22 +20,22 @@ CREATE TABLE cards(
     notes_on_subject TEXT,
     display_name_visible BOOLEAN DEFAULT false,
     date_of_birth_visible BOOLEAN DEFAULT false,
-    ciudad_visible BOOLEAN DEFAULT false,
-    pais_visible BOOLEAN DEFAULT false,
-    foto_visible BOOLEAN DEFAULT false,
+    city_visible BOOLEAN DEFAULT false,
+    country_visible BOOLEAN DEFAULT false,
+    photo_visible BOOLEAN DEFAULT false,
     bio_visible BOOLEAN DEFAULT false,
-    intereses_visible BOOLEAN DEFAULT false,
-    idiomas_visible BOOLEAN DEFAULT false,
+    hobbies_visible BOOLEAN DEFAULT false,
+    languages_visible BOOLEAN DEFAULT false,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP NOT NULL DEFAULT now()
 );
 ALTER TABLE users 
     ADD COLUMN display_name TEXT,
     ADD COLUMN bio TEXT,
-    ADD COLUMN ciudad TEXT,
-    ADD COLUMN pais TEXT,
-    ADD COLUMN intereses TEXT,
-    ADD COLUMN idiomas TEXT;
+    ADD COLUMN city TEXT,
+    ADD COLUMN country TEXT,
+    ADD COLUMN hobbies TEXT,
+    ADD COLUMN languages TEXT;
 
 CREATE TABLE user_photos(
     photo_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -52,5 +52,11 @@ CREATE TABLE user_photos(
 -- +goose Down
 DROP TABLE cards;
 DROP TABLE user_photos;
-ALTER TABLE users DROP COLUMN display_name, bio, ciudad, pais, intereses, idiomas;
+ALTER TABLE users 
+    DROP COLUMN display_name,
+    DROP COLUMN bio,
+    DROP COLUMN city,
+    DROP COLUMN country,
+    DROP COLUMN hobbies,
+    DROP COLUMN languages;
 
